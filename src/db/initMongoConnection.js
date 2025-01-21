7d0a2e5cd64c8efd91a48bccb69073c09ca96b8f
Katya <katyakhomenko2105@gmail.com>
2025-01-21 19:26:37 +0200

    commit hw

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -4,12 +4,14 @@ import { env } from '../utils/env.js';
 export const initMongoConnection = async () => {
   try {
     const user = env('MONGODB_USER');
-    const pwd = env('MONGODB_PASSWORD');
+    const pwd = encodeURIComponent(env('MONGODB_PASSWORD'));
     const url = env('MONGODB_URL');
     const db = env('MONGODB_DB');
 
+    console.log({ user, pwd, url, db });
+
     await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}${url}/${db}?retryWrites=true&w=majority`,
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
     );
 
     console.log('Mongo connection successfully established!');
