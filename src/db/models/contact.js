7d53e94bc05081f6f7e62bde0a2e46bee7059821
Katya <katyakhomenko2105@gmail.com>
2025-01-23 01:03:33 +0200

    one commit

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -1,6 +1,6 @@
-import { model, Schema } from 'mongoose';
+import { Schema, model } from 'mongoose';
 
-const contactSchema = new Schema(
+const ContactSchema = new Schema(
   {
     name: {
       type: String,
@@ -12,21 +12,17 @@ const contactSchema = new Schema(
     },
     email: {
       type: String,
+      reqired: true,
     },
-    isFavourite: {
-      type: Boolean,
-      default: false,
-    },
+    isFavourite: { type: Boolean, default: false },
     contactType: {
       type: String,
       enum: ['work', 'home', 'personal'],
+      required: true,
       default: 'personal',
     },
   },
-  {
-    timestamps: true,
-    versionKey: false,
-  },
+  { timestamps: true, versionKey: false },
 );
 
-export const ContactsCollection = model('contacts', contactSchema);
+export const ContactsColection = model('contacts', ContactSchema);
