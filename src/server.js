6b374568961ef3208fbd1cec9a2b995a9ddf9f19
Katya <katyakhomenko2105@gmail.com>
2025-01-25 16:12:31 +0200

    commit hw

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import pinoHttp from 'pino-http';
-import pino from 'pino';
+import pino from 'pino-http';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-// import ContactsRouter from './routers/contacts.js';
+
 import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+// import { UPLOAD_DIR } from './constants/constants.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,13 +15,16 @@ export const setupServer = () => {
   const app = express();
   app.use(express.json());
 
-  const logger = pino({
-    level: process.env.LOG_LEVEL || 'info',
-    transport: { target: 'pino-pretty' },
-  });
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
   app.use(cors());
-  app.use(pinoHttp({ logger }));
+
   app.use(cookieParser());
 
   app.use(router);
